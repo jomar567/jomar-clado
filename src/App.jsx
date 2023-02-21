647f29239a6002e11c6634b21f027fc460d15411
jomar567 <cladojomar881@gmail.com>
2023-02-21 18:42:12 +0800

    tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header flex justify-center items-center">
         <a
           className="App-link"
           href="https://reactjs.org"
