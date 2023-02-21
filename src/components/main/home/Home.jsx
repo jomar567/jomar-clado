aaad9c1910adf2bcc9781ba8577d022edcd40f6f
jomar567 <cladojomar881@gmail.com>
2023-02-22 00:46:23 +0800

    social icon banner

--- src/components/main/home/Home.jsx
+++ src/components/main/home/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import {Typewriter, Cursor} from 'react-simple-typewriter'
+import { Typewriter } from 'react-simple-typewriter'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const Home = () => {
   return (
-      <div className='absolute flex justify-center items-center min-h-screen w-full px-3'>
-      <div className="container mx-auto">
+    <section>
+      <div className='py-20 md:py-0 absolute flex justify-center items-center min-h-screen w-full px-3'>
+      <div className="container mx-auto relative">
         <div className='grid md:grid-cols-2 gap-5'>
           <div className='text-center md:text-left flex flex-col justify-center'>
-            <h1 className='text-3xl font-medium'>HELLO, I AM</h1>
-            <h1 className='text-blue-secondary text-6xl font-semibold py-4'>
+            <h1 className='md:text-3xl text-2xl font-medium'>HELLO, I AM</h1>
+            <h1 className='text-blue-secondary md:text-6xl text-5xl font-semibold py-4'>
               <Typewriter
                 words={['JOMAR CLADO', 'WEB DEVELOPER', 'CODER']}
                 loop={0}
@@ -18,19 +21,36 @@ const Home = () => {
                 delaySpeed={1000}
               />
             </h1>
-            <p className=' text-base'>I am a web developer with 3 years of experience <br />
+            <p className=' md:text-base text-sm'>I am a web developer with 3 years of experience <br />
               specializing in front-end development.</p>
-            <button className=' bg-blue-secondary text-base text-white py-2.5 px-7 mr-4 rounded-lg mt-4 w-fit'>
+            <button className=' bg-blue-secondary text-base text-white py-2.5 px-7 mx-auto block md:mx-0 rounded-lg my-4 w-fit'>
               Let's Talk
             </button>
+            {/* Social Media */}
+          <p className='lg:absolute right-0'>
+            <span className='hidden md:block'>Follow me:</span>
+            <span className='text-blue-secondary'>
+              <a href="">
+                <FontAwesomeIcon icon={brands('linkedin')} className=" text-lg cursor-pointer" />
+              </a>
+              <a href="">
+                <FontAwesomeIcon icon={brands('github')} className=" text-lg cursor-pointer mx-5 md:mx-0" />
+              </a>
+              <a href="">
+                <FontAwesomeIcon icon={brands('facebook')} className=" text-lg cursor-pointer" />
+              </a>
+            </span>
+          </p>
           </div>
 
           <div className='flex justify-center items-center'>
             <img src={require('../../../assets/img/BannerImg.png')} alt="Profile" />
           </div>
+          
         </div>
       </div>
       </div>
+    </section>
   )
 }
 
