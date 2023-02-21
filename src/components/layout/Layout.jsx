075f4c080996a1e9f3dbce16541f79830148b5b8
jomar567 <cladojomar881@gmail.com>
2023-02-21 19:09:05 +0800

    installed and configure fontawesome icons

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const Layout = () => {
   return (
-    <div className="flex justify-center items-center">Layout</div>
+    <div className="flex justify-center items-center">Layout
+    <FontAwesomeIcon className="m-1 fs-3 text-primary" icon={brands('facebook')} />
+    <FontAwesomeIcon className="m-1 fs-3 text-primary" icon={brands('linkedin')} />
+    <FontAwesomeIcon className="m-1 fs-3 text-primary" icon={brands('twitter')} /></div>
   )
 }
 
