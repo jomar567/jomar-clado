a366d7811849c4dea21b61ffd3f95dd40ed330f3
bolimajohnmark <bolimajohnmark270@gmail.com>
2023-02-27 19:33:31 +0800

    fixed toggleable

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = ({ handleClick, activeDark }) => {
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
             </button>
           </div>
-          <div className={`${navShow ? 'hidden' : 'block'} w-full md:block md:w-auto md:order-1`}>
+          <div className={`${navShow ? 'block' : 'hidden'} w-full md:block md:w-auto md:order-1`}>
             <ul className="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               <li>
                 <a href="#home" onClick={() => setActiveNav('#home')} className={`${activeNav === '#home' ? 'active' : ''} block py-4 pl-3 pr-4 text-gray bg-blue md:p-0 hover:text-blue-secondary dark:text-blue-secondary`}>
